1b56f6c7d64e770b2d8e6fb73329622dd10e1474
alnarolekoala <valentin.frain458@laposte.net>
2021-07-10 15:29:08 +0200

    Update 10/07/21 ajout d'un dossier Svg

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -1,40 +1,146 @@
+// map world
+const mapW = document.getElementById("svg1");
+let pathsW = mapW.getElementsByTagName('path');
+// la map afrique SVG 
 const map = document.getElementById("svg2");
 let  paths = map.getElementsByTagName('path');
+// bouton filtre
 const filtreButton = document.getElementById('filtreButton');
 const filtre = document.getElementById('filtre');
+// contenu de l'info bulle
 let infoBulle = document.getElementById('description');
+// champ pour vérifier si on doit ouvrir ou non une modal 
 const verif = document.getElementById('verif').value;
-//  élément des modals de connexion/inscription
-
-const btnCloseModalI = document.getElementById('btnCloseModalI');
-const btnCloseModalC = document.getElementById('btnCloseModalC');
-const modalI = document.getElementById('modal-inscription');
-const modalC = document.getElementById('modal-connexion')
-const btnInscription = document.getElementById('btn-inscription');
-const btnConnexion = document.getElementById('btnConnexion');
-const userNameConnexion = document.getElementById('userNameConnexion');
-const userNameInscription = document.getElementById('userNameInscription');
-const userPasswordConnexion = document.getElementById('userPasswordConnexion');
-const userPasswordInscription = document.getElementById('userPasswordInscription');
-const userMailInscription = document.getElementById('userMailInscription'); 
-
 
 const body = document.body;
+// position de la souris axe X et Y 
 let Mouse_X; 
 let Mouse_Y;
+//  élément des modals de connexion/inscription
+// bouton de fermeture de la modal d'inscription
+const btnCloseModalI = document.getElementById('btnCloseModalI');
+// bouton de fermeture de la modal de connexion
+const btnCloseModalC = document.getElementById('btnCloseModalC');
+// modal inscription
+const modalI = document.getElementById('modal-inscription');
+// modal connexion
+const modalC = document.getElementById('modal-connexion');
+// bouton d'inscription
+const btnInscription = document.getElementById('btn-inscription');
+// bouton de connexion
+const btnConnexion = document.getElementById('btnConnexion');
+// champ userName dans la modal connexion
+const userNameConnexion = document.getElementById('userNameConnexion');
+// champ userName dans la modal d'inscription
+const userNameInscription = document.getElementById('userNameInscription');
+// champ password dans la modal de connexion
+const userPasswordConnexion = document.getElementById('userPasswordConnexion');
+// champ password dans la modal d'inscription
+const userPasswordInscription = document.getElementById('userPasswordInscription');
+// champ userMail dans la modal d'inscription
+const userMailInscription = document.getElementById('userMailInscription'); 
+// checkbox particulier dans la modal d'inscription
+const particulierCheck = document.getElementById('particulierCheck');
+// check box professionnel dans la modal d'inscription
+const professionnelCheck = document.getElementById('professionnelCheck');
+//bouton de changement de carte
+const partenaire = document.getElementById('partenaire');
+const projet = document.getElementById('projet');
+const prestataire = document.getElementById('prestataire');
+// div conteneur des svg
+const conteneurMap = document.getElementById('map');
+const conteneurMapWorld = document.getElementById('mapWorld'); 
+//écoute sur les boutons de changement de carte 
+
+partenaire.addEventListener('click', function() {
+  conteneurMapWorld.removeAttribute('hidden'); 
+  conteneurMap.setAttribute('hidden', 'hidden');
+  document.getElementById('searchZone').classList.remove('col-md-5');
+  document.getElementById('searchZone').classList.add('col-md-7');
+  partenaire.classList.remove('btn-primary');
+  partenaire.classList.add('btn-danger');
+
+  if(projet.classList.contains('btn-danger')){
+    projet.classList.remove('btn-danger');
+    projet.classList.add('btn-primary');
+  }
+  if(prestataire.classList.contains('btn-danger')){
+    prestataire.classList.remove('btn-danger');
+    prestataire.classList.add('btn-primary');
+  }
+  
+})
+
+projet.addEventListener('click', function(){
+  conteneurMap.removeAttribute('hidden'); 
+  conteneurMapWorld.setAttribute('hidden', 'hidden');
+  document.getElementById('searchZone').classList.remove('col-md-7');
+  document.getElementById('searchZone').classList.add('col-md-5');
+
+  projet.classList.remove('btn-primary');
+  projet.classList.add('btn-danger');
+  
+  if(partenaire.classList.contains('btn-danger')){
+    partenaire.classList.remove('btn-danger');
+    partenaire.classList.add('btn-primary');
+  }
+  if(prestataire.classList.contains('btn-danger')){
+    prestataire.classList.remove('btn-danger');
+    prestataire.classList.add('btn-primary');
+  }
+})
+
+prestataire.addEventListener('click', function(){
+  conteneurMap.removeAttribute('hidden'); 
+  conteneurMapWorld.setAttribute('hidden', 'hidden');
+  document.getElementById('searchZone').classList.remove('col-md-7');
+  document.getElementById('searchZone').classList.add('col-md-5');
+
+  prestataire.classList.remove('btn-primary');
+  prestataire.classList.add('btn-danger');
+  
+  if(projet.classList.contains('btn-danger')){
+    projet.classList.remove('btn-danger');
+    projet.classList.add('btn-primary');
+  }
+  if(partenaire.classList.contains('btn-danger')){
+    partenaire.classList.remove('btn-danger');
+    partenaire.classList.add('btn-primary');
+  }
+})
+
+// écoute du changement sur la checkbox professionnel
+professionnelCheck.addEventListener('change', function() {
+
+    // si professionnel est coché décoche particulier
+    if(professionnelCheck.checked == true){
+      particulierCheck.checked = false;
+    }
+
+
+})
+// écoute du changement sur la checkbox particulier
+particulierCheck.addEventListener('change', function() {
+  // si particulier est coché décoche professionnel
+  if(particulierCheck.checked == true){
+    professionnelCheck.checked = false;
+  }
+
+})
 
 
 
+// écoute du chargement de la page 
 
 document.addEventListener('DOMContentLoaded', function() {
   
-  if(verif == "error"){
+  if(verif == "1"){
     btnInscription.click();
     verif= "";
   }
  
 });
-
+// écoute sur le bouton de fermeture de la modal de connexion 
 btnCloseModalC.addEventListener('click', function () {
   userNameConnexion.value = "";
   userPasswordConnexion.value = "";
@@ -42,7 +148,7 @@ btnCloseModalC.addEventListener('click', function () {
   
 
 })
-
+// écoute sur le bouton de fermeture de la modal d'inscription 
 btnCloseModalI.addEventListener('click', function () {
   modalI.style.display = 'none';
   userNameInscription.value = "";
@@ -53,13 +159,13 @@ btnCloseModalI.addEventListener('click', function () {
 })
 
 
-
+// écoute du bouton d'inscription
 btnInscription.addEventListener('click', function () {
   
   modalI.style.display = 'block';
 
 })
-
+// écoute sur le bouton de connexion
 btnConnexion.addEventListener('click', function () {
   
   modalC.style.display = 'block';
@@ -88,7 +194,7 @@ filtreButton.addEventListener('click', function(){
     }
     
 })
-
+// fonction qui récupere la position de la souris et actualise la position de l'infoBulle en conséquence
 function RefreshPosition() {
   // on récupere la position X de la souris a laquelle on ajoute 5 
   var X = parseInt(document.getElementById('imp1').value)+5;
@@ -98,17 +204,10 @@ function RefreshPosition() {
   infoBulle.style.left = X+"px";
   // on défini la position Y de l'infoBulle
   infoBulle.style.top = Y+"px";
-// on récupere la position X de la souris a laquelle on ajoute 5 
-      var X = parseInt(document.getElementById('imp1').value)+5;
-      // on récupere la position Y de la souris a laquelle on ajoute 5
-      var Y = parseInt(document.getElementById('imp2').value)+5;
-      // on défini la position X de l'infoBulle'
-      infoBulle.style.left = X+"px";
-      // on défini la position Y de l'infoBulle
-      infoBulle.style.top = Y+"px";
 }
 
 
+// actualisation de l'infoBulle pour la afrique ==================================================================================================================================================
 for (var i = 0; i < paths.length; i++) {
   // quand la souris entre dans un pays 
   paths[i].addEventListener("mouseover", function(e){
@@ -137,7 +236,37 @@ for (var i = 0; i < paths.length; i++) {
     pays.setAttribute("fill", "#FFE4B5");
   })
 }
+//======================================================================================================================================================================================================
+// actualisation de l'infoBulle pour la mapWorld========================================================================================================================================================
+for (var i = 0; i < pathsW.length; i++) {
+  // quand la souris entre dans un pays 
+  pathsW[i].addEventListener("mouseover", function(e){
+    let pays  = e.target;
+    // On met le pays selectionné en rouge
+    pays.setAttribute("fill", "red");
+    // on entre dans l'infoBulle l'id qui correspond au nom du pays 
+    infoBulle.innerHTML = e.target.classList;
+    // on rend l'info bulle visible
+    infoBulle.removeAttribute('hidden');
+    // permet de donner une position a l'infoBulle a chaque deplacement de souris
+    body.addEventListener("mousemove", function(e){
+      RefreshPosition();
+    })
+  })
+}
+  
+for (var i = 0; i < pathsW.length; i++) {
 
+  // quand la souris sort d'un pays
+  pathsW[i].addEventListener("mouseout", function(e){
+    let pays  = e.target;
+    // on rend l'infoBulle invisible
+    infoBulle.setAttribute('hidden', "hidden")
+    // on remet le pays dans sa couleur de base
+    pays.setAttribute("fill", "#FFE4B5");
+  })
+}
+//======================================================================================================================================================================================================
 // Position de la souris enregistrer en permanence
 function WhereMouse( evt ){
 
--- scriptPhp/inscription.php
+++ scriptPhp/inscription.php
@@ -5,17 +5,52 @@ $userPasswordInscription = $_POST['userPasswordInscription'];
 $userMailInscription = $_POST['userMailInscription'];
 $regexMail;
 $regexStandard;
-$msgError;
+$msgError = array();
 
-if(empty($userNameInscription) || empty($userPasswordInscription) || empty($userMailInscription)){
+// si aucune checkbox est coché
+if (!(isset($_POST['professionnelCheck'])) && !(isset($_POST['particulierCheck'])) ) {
+    $msgError[3] = "Veuillez indiquer si vous êtes un particulier ou un professionnel";
+  }
+  else {
+    if ((isset($_POST['professionnelCheck'])) && (isset($_POST['particulierCheck'])) ) {
+        $msgError[3] = "Veuillez choisir si vous êtes un particulier ou un professionnel(modifiable par la suite)";
+    }
+    else{
+        if(isset($_POST['professionnelCheck'])){
+            $check = "1";
+        }
+        if(isset($_POST['particulierCheck'])){
+            $check ="2";
+        }
+    }
+  }
+
+if(empty($userNameInscription)) {
  
-    $msgError = "error";
+    $msgError[0] = "Le champ Identifiant est vide";
+}
+
+if(empty($userPasswordInscription)) {
+    $msgError[1] = "Le champ mot de passe est vide";
+}
+
+if(empty($userMailInscription)){
+    $msgError[2] = "Le champ Email est vide";
+}
+
+if(!empty($msgError)){
+    $verif = "1";
+}
+else {
+    $verif = "2";
 }
 
 
 
 
-header("Location: ../index.php?msgError=$msgError"); /* Redirection du navigateur */
+
+
+header("Location: ../index.php?verif=$verif&msgError=$msgError[0]&msgError1=$msgError[1]&msgError2=$msgError[2]&msgError3=$msgError[3]"); /* Redirection du navigateur */
 
 /* Assurez-vous que la suite du code ne soit pas exécutée une fois la redirection effectuée. */
 exit;
